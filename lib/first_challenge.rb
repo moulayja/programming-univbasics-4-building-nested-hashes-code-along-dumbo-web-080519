5a72c0ff691b0fbc3b688b72c6c72fd86bbfdfe0
Jamal Farah <moulayjam@gmail.com>
2019-06-23 19:04:39 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,18 @@
-def first_challenge
-   epic_tragedy = {}
+def hopper
+	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     programmer_hash[:grace_hopper]
 end
